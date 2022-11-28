d57a5f154344c8073158f75bbb437031b0cc2a94
raselrk5588 <rasel.info5588@gmail.com>
2022-11-28 21:37:58 +0600

    hello world

--- Index.java
+++ Index.java
@@ -1,7 +1,18 @@
 public class Main {
 
-  public static void main(String[] args) {
-  	System.out.println("Hello, World!"); 
-  }
+//   public static void main(String[] args) {
+//   	System.out.println("Hello, World!"); 
+//   }
+  private Plan createPlan() {
+    return new Plan(
+            project(),
+            "Plan Name", "PLANKEY")
+            .description("Plan created from (enter repository url of your plan)")
+            .stages(
+                    new Stage("Stage 1")
+                            .jobs(new Job("Run", "RUN")
+                                    .tasks(
+                                            new ScriptTask().inlineBody("echo Hello world!"))));
+}
   
 }
